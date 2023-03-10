4d45026ffd4f420f18299f760f7bba6079daf929
Daniel Rocha <danielsantos852@gmail.com>
2023-03-10 16:14:44 -0300

    Added tolerance to 5xx API errors

--- scripts/news.py
+++ scripts/news.py
@@ -1,6 +1,8 @@
 # Import libraries
 import requests as re
 import json
+import time
+import sys
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
@@ -21,6 +23,8 @@ queries = [
 
 # API parameters
 api_key = '10940c331d2a782a7a7181a67d0c9b9b'
+time_between_retries = 10
+max_retries = 3
 
 # Export file parameters
 filename_output = '1.raw'
@@ -40,13 +44,49 @@ def main() -> None:
         # Set API URL
         api_url = f"https://gnews.io/api/v4/search?q={query['query']}&apikey={api_key}"
 
-        # Get response from API
-        response = get_api_response(url=api_url,
-                                    lang=query['language'],
-                                    country=query['country'])
-    
-        # [DEBUG] Get mock response
-        #response = get_mock_response()
+        # Start retry counter
+        retries = 0
+
+        # Start infinite loop
+        while True:
+            
+            # Get response from API
+            response, response_code = get_api_response(url=api_url,
+                                                       lang=query['language'],
+                                                       country=query['country'])
+            
+            # If Error 500:
+            if response_code == 500:
+                
+                # Print error message
+                print('ERROR 500: Internal API Server Error. ', end='')
+            
+            # Else, if Error 503:
+            elif response_code == 503:
+                
+                # Print error message
+                print(f'ERROR 503: Service Unavailable. ', end='')
+
+            # Else:
+            else:
+            
+                # Break from the infinite loop
+                break                
+            
+            # Update retry counter
+            retries += 1
+            
+            # If max retries reached:
+            if retries > max_retries:
+                
+                # Exit program with "max retries reached" message
+                sys.exit('Maximum amount of retries reached. Stopping program...')
+            
+            # Print retry message
+            print(f'Retrying in {time_between_retries} seconds. (Retry {retries} of {max_retries})')
+
+            # Wait for [time_between_retries] seconds
+            time.sleep(time_between_retries)
         
         # [DEBUG] Export response as JSON file
         #export_json(d=response, filename=f"mock_{query['query']}_{query['language']}_{query['country']}")
@@ -79,6 +119,9 @@ def get_api_response(url:str, lang:str, country:str) -> dict:
                             #'to':'2023-03-08T00:00:00Z',
                             'sortby':'publishedAt',})            # 'relevance' or 'publishedAt'
     
+    # Get response code from API response
+    r_code = r.status_code
+    
     # Get response content (as bytes class)
     r = r.content
     
@@ -89,13 +132,13 @@ def get_api_response(url:str, lang:str, country:str) -> dict:
     r = json.loads(r)
     
     # Return response content
-    return r
+    return r, r_code
 
 
 # Get Mock Response function
 def get_mock_response() -> dict:
     
-    ''' Returns a mock response for delevoping purposes. '''
+    ''' Returns a mock response (for delevoping purposes). '''
     
     # Load response data from mock JSON file
     with open('./data/mock.json', 'r', encoding='utf-8') as file:
